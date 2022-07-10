490d855ead2afbad42bf3b38c52457598de93c26
Jeff Jones <jeffreydonovanjones@gmail.com>
2022-07-10 12:09:45 -0400

    added cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const indexRouter = require("./routes/index");
 const employeesRouter = require("./routes/employees");
 
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
